dd9647589417fb6aebe547b71b290e96b7c6ad08
Turbotondel <tim.dekruijf1998@gmail.com>
2017-05-18 10:10:28 +0200

    Added DML file for the experiment

---
+++ SQL/Experiment DML.sql
@@ -0,0 +1,9 @@
+USE SQL_Experiment
+GO
+
+INSERT INTO Users(voornaam, tussenvoegsel, achternaam, username) VALUES ('Pietje', null, 'Schouten', 'PS_Socialcoin'),
+																		('Tim', null, 'Hendriksen', 'Timo The Jus')
+
+INSERT INTO Wallet VALUES(1, 100),
+						 (2, 0)
+
